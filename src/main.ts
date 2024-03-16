e663a882a06aeade24b394b4239404c080d2ad82
do-huni <orangnlp@gmail.com>
2024-03-16 02:12:28 +0000

    chore: docker port revised

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(3000);
+  await app.listen(`${process.env.SERVER_PORT}`);
 }
 bootstrap();
