e6f0ac80d1237002c3ee59ffd8991a7610612b94
do-huni <orangnlp@gmail.com>
2024-03-17 14:04:04 +0000

    Fix: fetch-likes-response dto

--- src/APIs/likes/dto/fetch-likes-response.dto.ts
+++ src/APIs/likes/dto/fetch-likes-response.dto.ts
@@ -2,6 +2,6 @@ import { Like } from '../entities/like.entity';
 import { ApiProperty, PickType } from '@nestjs/swagger';
 
 export class FetchLikesResponseDto extends PickType(Like, ['id', 'user']) {
-  @ApiProperty({ description: '포스트 아이디', type: Number })
-  post: number;
+  //   @ApiProperty({ description: '포스트 아이디', type: Number })
+  //   post: number;
 }
