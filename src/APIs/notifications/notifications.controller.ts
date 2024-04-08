7016e1a85937503f93eba9b09b29f518baede684
do-huni <119511836+do-huni@users.noreply.github.com>
2024-04-08 11:35:45 +0900

    feat: predev basic notification ops

--- src/APIs/notifications/notifications.controller.ts
+++ src/APIs/notifications/notifications.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, Sse } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
 
-@Controller()
+@ApiTags('알림 API')
+@Controller('nots')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  /*
+  로직: 유저가 접속하면 알림을 구독한다.
+  최초 구독 시 알림을 한번 fetch 한다. getManyAndCount로 개수 체크해서 메인에 띄워줌
+  댓글을 달거나 등 알림이 생기면 kakaoId 옵저버에 next로 이벤트 갱신해준다.
+  갱신이 발생하면 sub해둔 프론트가 refetching을 한다.
+  브라우저를 끄거나 refetching이 한동안 일어나지 않으면 sse를 끊는다.
+  */
+  @ApiOperation({
+    summary: '[SSE] kakaoId로 오는 알림을 구독한다.',
+    description: '[swagger 불가능, postman 권장]',
+  })
+  @Sse(':kakaoId')
+  sendClientAlarm(@Param('kakaoId') kakaoId: number, @Req() req: Request) {
+    // this.notificationsService.addStream(this.users$, this.observer, userId);
+    // req.on('close', () =>
+    //   this.notificationsService.removeStream(req['user'].id.toString()),
+    // );
+    // return this.notificationsService.sendClientAlarm(+kakaoId);
+  }
+
+  @Get('send/:kakaoId')
+  test(@Param('kakaoId') kakaoId: number) {
+    // this.notificationsService.emitAlarm(kakaoId);
+  }
 }
--- src/APIs/notifications/notifications.service.ts
+++ src/APIs/notifications/notifications.service.ts
@@ -1,9 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, MessageEvent } from '@nestjs/common';
 import { NotificationsRepository } from './notifications.repository';
+import { Observable, ReplaySubject, Subject, filter, map } from 'rxjs';
 
 @Injectable()
 export class NotificationsService {
   constructor(
     private readonly notificationsRepository: NotificationsRepository,
   ) {}
+
+  // // [RxJS] Subject 선언 타입은 Users이다
+  // private users$: Subject<Notification> = new Subject();
+
+  // // 앞서 선언한 Subjcet를 Observable한 객체로 선언
+  // private observer = this.users$.asObservable();
+
+  // //접속한 브라우저의 커넥션을 담을 객체
+  // private stream: {
+  //   id: string;
+  //   subject: ReplaySubject<unknown>;
+  //   observer: Observable<unknown>;
+  // }[] = [];
+
+  // // 예시 데이터 ( DB 데이터라고 생각하자 )
+  // users = [
+  //   {
+  //     id: 1,
+  //     nickname: 'jewon',
+  //     level: 1,
+  //   },
+  //   { id: 2, nickname: 'je', level: 2 },
+  //   {
+  //     id: 3,
+  //     nickname: 'won',
+  //     level: 3,
+  //   },
+  // ];
+
+  // // [RxJS] User의 레벨 변화를 감시할 함수, 레벨업이 진행되면 Observable한 Subject에 next로 push
+  // // onUserLevelChange(userId: number, nickname: string, level: number) {
+  // //   // this.users$.next({ id: userId, nickname, level });
+  // // }
+
+  // //브라우저가 접속할 때 해당 스트림을 담아 둡니다.
+  // addStream(
+  //   subject: ReplaySubject<unknown>,
+  //   observer: Observable<unknown>,
+  //   id: string,
+  // ): void {
+  //   this.stream.push({
+  //     id,
+  //     subject,
+  //     observer,
+  //   });
+  // }
+  // // emitAlarm(kakaoId: number) {
+  //   // next를 통해 이벤트를 생성
+  //   this.users$.next({ kakaoId });
+  // }
+
+  // sendClientAlarm(kakaoId: number): Observable<any> {
+  //   // 이벤트 발생시 처리 로직
+  //   return this.observer.pipe(
+  //     // 유저 필터링
+  //     filter((user) => user.kakaoId === kakaoId),
+  //     // 데이터 전송
+  //     map((user) => {
+  //       return {
+  //         data: {
+  //           message: '알람이 발생했습니다.',
+  //         },
+  //       } as MessageEvent;
+  //     }),
+  //   );
+  // }
 }
