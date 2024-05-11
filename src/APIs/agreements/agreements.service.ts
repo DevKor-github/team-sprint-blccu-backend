94460f418622f9261f1eb6656897f8eb347cd412
do-huni <orangnlp@gmail.com>
2024-05-11 17:52:24 +0900

    feat: fetch agreements APIs

--- src/APIs/agreements/agreements.service.ts
+++ src/APIs/agreements/agreements.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { AgreementsRepository } from './agreements.repository';
 import { IAgreementsServiceCreate } from './interfaces/agreements.service.interface';
+import { FetchAgreementDto } from './dtos/fetch-agreement.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AgreementsService {
-  constructor(private readonly agreementsRepository: AgreementsRepository) {}
+  constructor(
+    private readonly agreementsRepository: AgreementsRepository,
+    private readonly usersService: UsersService,
+  ) {}
 
-  async create({ kakaoId, agreementType, isAgreed }: IAgreementsServiceCreate) {
-    await this.agreementsRepository.save({
+  async adminCheck({ kakaoId }) {
+    await this.usersService.adminCheck({ kakaoId });
+  }
+
+  async create({
+    kakaoId,
+    agreementType,
+    isAgreed,
+  }: IAgreementsServiceCreate): Promise<FetchAgreementDto> {
+    return await this.agreementsRepository.save({
       agreementType,
       isAgreed,
       user: { kakaoId },
     });
+  }
+
+  async fetchAll({ kakaoId }): Promise<FetchAgreementDto[]> {
+    return await this.agreementsRepository.find({ where: { user: kakaoId } });
   }
 }
