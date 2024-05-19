cd2111f76d79b65aac70b8ade6c65c89c4d04be7
do-huni <orangnlp@gmail.com>
2024-05-19 21:01:17 +0900

    fix: accuracy of follow list isFollowing

--- src/APIs/follows/follows.repository.ts
+++ src/APIs/follows/follows.repository.ts
@@ -9,11 +9,38 @@ export class FollowsRepository extends Repository<Follow> {
   }
 
   async getFollowers({ kakaoId, loggedUser }) {
-    const queryBuilder = this.getFollowQuery({ kakaoId, loggedUser });
-    const followings = await queryBuilder
+    const followings = await this.createQueryBuilder('follow')
       .innerJoin('follow.from_user', 'user')
+      .where('user.date_deleted IS NULL')
       .andWhere('follow.toUserKakaoId = :kakaoId')
+      .leftJoinAndSelect(
+        (subQuery) => {
+          return subQuery
+            .select('follow2.toUserKakaoId', 'toUserKakaoId')
+            .from(Follow, 'follow2')
+            .where('follow2.fromUserKakaoId = :loggedUser');
+        },
+        'follow2',
+        'follow2.toUserKakaoId = user.kakaoId',
+      )
+      .select([
+        'user.username AS username',
+        'user.kakaoId AS kakaoId',
+        'user.handle AS handle',
+        'user.follower_count AS follower_count',
+        'user.following_count AS following_count',
+        'user.isAdmin AS isAdmin',
+        'user.username AS username',
+        'user.description AS description',
+        'user.profile_image AS profile_image',
+        'user.background_image AS background_image',
+        'user.date_created AS date_created',
+        'user.date_deleted AS date_deleted',
+        'CASE WHEN follow2.toUserKakaoId IS NOT NULL THEN true ELSE false END AS isFollowing',
+      ])
+      .setParameters({ kakaoId, loggedUser })
       .getRawMany();
+
     return followings.map((follower) => ({
       username: follower.username,
       kakaoId: follower.kakaoId,
@@ -31,11 +58,38 @@ export class FollowsRepository extends Repository<Follow> {
   }
 
   async getFollowings({ kakaoId, loggedUser }) {
-    const queryBuilder = this.getFollowQuery({ kakaoId, loggedUser });
-    const followings = await queryBuilder
+    const followings = await this.createQueryBuilder('follow')
       .innerJoin('follow.to_user', 'user')
+      .where('user.date_deleted IS NULL')
       .andWhere('follow.fromUserKakaoId = :kakaoId')
+      .leftJoinAndSelect(
+        (subQuery) => {
+          return subQuery
+            .select('follow2.toUserKakaoId', 'toUserKakaoId')
+            .from(Follow, 'follow2')
+            .where('follow2.fromUserKakaoId = :loggedUser');
+        },
+        'follow2',
+        'follow2.toUserKakaoId = user.kakaoId',
+      )
+      .select([
+        'user.username AS username',
+        'user.kakaoId AS kakaoId',
+        'user.handle AS handle',
+        'user.follower_count AS follower_count',
+        'user.following_count AS following_count',
+        'user.isAdmin AS isAdmin',
+        'user.username AS username',
+        'user.description AS description',
+        'user.profile_image AS profile_image',
+        'user.background_image AS background_image',
+        'user.date_created AS date_created',
+        'user.date_deleted AS date_deleted',
+        'CASE WHEN follow2.toUserKakaoId IS NOT NULL THEN true ELSE false END AS isFollowing',
+      ])
+      .setParameters({ kakaoId, loggedUser })
       .getRawMany();
+
     return followings.map((follower) => ({
       username: follower.username,
       kakaoId: follower.kakaoId,
@@ -50,28 +104,5 @@ export class FollowsRepository extends Repository<Follow> {
       date_deleted: follower.date_deleted,
       isFollowing: follower.isFollowing === 1, // MySQL에서는 boolean 값이 1 또는 0으로 반환될 수 있음
     }));
-  }
-
-  getFollowQuery({ kakaoId, loggedUser }) {
-    const queryBuilder = this.createQueryBuilder('follow')
-      .where('user.date_deleted IS NULL')
-      .select([
-        'user.username AS username',
-        'user.kakaoId AS kakaoId',
-        'user.handle AS handle',
-        'user.follower_count AS follower_count',
-        'user.following_count AS following_count',
-        'user.isAdmin AS isAdmin',
-        'user.username AS username',
-        'user.description AS description',
-        'user.profile_image AS profile_image',
-        'user.background_image AS background_image',
-        'user.date_created AS date_created',
-        'user.date_deleted AS date_deleted',
-        'CASE WHEN follow.fromUserKakaoId = :loggedUser THEN true ELSE false END AS isFollowing',
-      ])
-      .setParameters({ kakaoId, loggedUser });
-
-    return queryBuilder;
   }
 }
