aec35fd17bc4ee7a66fdad11d972dc591a0ba18c
do-huni <orangnlp@gmail.com>
2024-07-06 19:21:03 +0900

    refactor(users): reorganize module structure

--- src/APIs/users/users.module.ts
+++ src/APIs/users/users.module.ts
@@ -1,15 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { ImagesModule } from 'src/modules/images/images.module';
+import { UsersCreateService } from './services/users-create.service';
+import { UsersReadService } from './services/users-read.service';
+import { UsersUpdateService } from './services/users-update.service';
+import { UsersDeleteService } from './services/users-delete.service';
+import { UsersCreateController } from './controllers/users-create.controller';
+import { UsersReadController } from './controllers/users-read.controller';
+import { UsersUpdateController } from './controllers/users-update.controller';
+import { UsersDeleteController } from './controllers/users-delete.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), ImagesModule],
-  providers: [UsersService, UsersRepository],
-  controllers: [UsersController],
-  exports: [UsersService],
+  providers: [
+    UsersCreateService,
+    UsersReadService,
+    UsersUpdateService,
+    UsersDeleteService,
+    UsersRepository,
+  ],
+  controllers: [
+    UsersCreateController,
+    UsersReadController,
+    UsersUpdateController,
+    UsersDeleteController,
+  ],
+  exports: [
+    UsersCreateService,
+    UsersReadService,
+    UsersUpdateService,
+    UsersDeleteService,
+  ],
 })
 export class UsersModule {}
