6dec03a5e0c211fdf8d293053a61d45b6bdcbd05
do-huni <orangnlp@gmail.com>
2024-09-25 22:36:14 +0900

    feat: s3 url 경로 변경

--- src/modules/images/interfaces/images.service.interface.ts
+++ src/modules/images/interfaces/images.service.interface.ts
@@ -2,6 +2,7 @@ export interface IImagesServiceUploadImage {
   file: Express.Multer.File;
   resize: number;
   ext: 'jpg' | 'png';
+  tag?: string;
 }
 
 export interface IImagesServiceDeleteImage {
